5e1c3cc1a3b40a0fc4ef3cfddb7b8118afb7db0f
Larissa Nunes <larissanunes0904@gmail.com>
2024-02-09 20:17:34 -0300

    first commit - criando as packages para o projeto inicial

---
+++ src/test/java/com/larissa/tcc2024/Tcc2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.tcc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tcc2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
